eb9f37e3fd32f1a33049782a14c2cbd1bbc53a8c
ShuvO9gi <shuvo.omar09@gmail.com>
2023-05-31 22:47:24 +0600

    Lesson-5 Source Code

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
 import "../styles/Global.css";
-import { Layout } from "./Layout";
-import Home from "./pages/Home";
+import Layout from "./Layout";
+import Signup from "./pages/Signup";
+//import Home from "./pages/Home";
 
 function App() {
   return (
     <>
       <div className="App">
         <Layout>
-          <Home />
+          {/* <Home /> */}
+          <Signup />
         </Layout>
       </div>
     </>
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,7 +1,7 @@
 import classes from "../styles/Layout.module.css";
 import { Nav } from "./Nav";
 
-export function Layout({ children }) {
+export default function Layout({ children }) {
   return (
     <>
       <Nav />
