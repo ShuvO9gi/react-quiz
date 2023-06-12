f95193953ef15c967592dc63fb8e2a1a3d1b2a41
ShuvO9gi <shuvo.omar09@gmail.com>
2023-06-12 19:42:16 +0600

    Lesson-12 Login Problem Solved

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -8,7 +8,7 @@ import TextInput from "./TextInput";
 
 export default function LoginForm() {
   const [email, setEmail] = useState("");
-  const [password, setPassword] = useState();
+  const [password, setPassword] = useState("");
   const [loading, setLoading] = useState();
   const [error, setError] = useState();
 
@@ -33,6 +33,7 @@ export default function LoginForm() {
   return (
     <Form style={{ height: "330px" }} onSubmit={handleLogin}>
       <TextInput
+        required
         type="text"
         placeholder="Enter email"
         icon="alternate_email"
@@ -40,13 +41,14 @@ export default function LoginForm() {
         onChange={(e) => setEmail(e.target.value)}
       />
       <TextInput
+        required
         type="password"
         placeholder="Enter password"
         icon="lock"
         value={password}
         onChange={(e) => setPassword(e.target.value)}
       />
-      <Button disabled={loading} type="button">
+      <Button disabled={loading} type="submit">
         <span>Submit Now</span>
       </Button>
       {error && <p className="error">{error}</p>}
@@ -54,3 +56,60 @@ export default function LoginForm() {
     </Form>
   );
 }
+
+/* export default function LoginForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [error, setError] = useState();
+  const [loading, setLoading] = useState();
+
+  const { login } = useAuth();
+  const navigate = useNavigate();
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    try {
+      setError("");
+      setLoading(true);
+      await login(email, password);
+      navigate("/");
+    } catch (err) {
+      console.log(err);
+      setLoading(false);
+      setError("Failed to login!");
+    }
+  }
+
+  return (
+    <Form style={{ height: "330px" }} onSubmit={handleSubmit}>
+      <TextInput
+        type="text"
+        placeholder="Enter email"
+        icon="alternate_email"
+        required
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <TextInput
+        type="password"
+        placeholder="Enter password"
+        icon="lock"
+        required
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <Button type="submit" disabled={loading}>
+        <span>Submit Now</span>
+      </Button>
+
+      {error && <p className="error">{error}</p>}
+
+      <Query text1="Don't have an account?" text2="Signup" href="/signup" />
+    </Form>
+  );
+}
+ */
